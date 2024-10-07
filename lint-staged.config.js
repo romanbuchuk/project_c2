e72e013a1c41a5bcb9c1754aab0ce9150211c05a
romanbuchuk <roman.buchuk@uzhnu.edu.ua>
2024-10-07 16:51:52 +0300

    Added lint-staged configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    '*.{js,jsx}': ['eslint --ext .js,.jsx --fix src/'],
+    '*.{js,jsx}': ['eslint --ignore-path .gitignore --fix --ext .js,.jsx --max-warnings 0 ./src'],
+    '*.{js,jsx,css,mjs}': ['prettier --write'],
 };
