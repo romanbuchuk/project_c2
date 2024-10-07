afc6368b39994f080118ac1eb91c79eccc47a84f
romanbuchuk <roman.buchuk@uzhnu.edu.ua>
2024-10-07 16:55:21 +0300

    Changed file for testing lint-staged

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-// import resizeHook from '@hooks/resize';
 
+// import resizeHook from '@hooks/resize';
 import styles from './App.css';
 // import * as styles from './App.css';
+
 const App = () => {
     return (
         <div className={styles.main} data-testid="app">
